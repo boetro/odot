04fb965033ced7e33dd496869ea9ef918a3bbada
Caleb Van Dyke <ctvandyke24@gmail.com>
2025-08-18 14:04:22 -0500

    update how often we call the /me endpoint

--- ui/src/routes/(app)/route.tsx
+++ ui/src/routes/(app)/route.tsx
@@ -5,8 +5,7 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/(app)")({
   component: RouteComponent,
   beforeLoad: async ({ context, location }) => {
-    const state = await context.auth.checkAuth();
-    if (!state.isAuthenticated) {
+    if (!context.auth.isAuthenticated) {
       throw redirect({
         to: "/login",
         search: {
